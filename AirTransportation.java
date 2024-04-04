a633f2c714c2f43a525c06d1b5fae4ffdbd9b465
Alexandre Caneca <57361392+nonordron@users.noreply.github.com>
2024-04-04 10:42:23 +0100

    AirTransportation Nivel 3

--- AirTransportation.java
+++ AirTransportation.java
@@ -23,6 +23,35 @@ public class AirTransportation extends Transport{
     }
 
     public void setNumberOfContainers(int numberOfContainers) {
-        this.numberOfContainers = numberOfContainers;
+        if(numberOfContainers >= 0)
+            this.numberOfContainers = numberOfContainers;
+    }
+
+
+    @Override
+    public double getPriceWithFees() {
+        return super.getPriceWithFees();
+    }
+
+    @Override
+    public String getTransportType() {
+        return "Transporte Aereo";
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n%15s: %s\n", "Tipo Transporte", getTransportType()));
+        sb.append(String.format("%15s: %s\n", "ID", super.getId()));
+        sb.append(String.format("%15s: %s\n", "Origem", super.getOrigin()));
+        sb.append(String.format("%15s: %s\n", "Destino", super.getDestination()));
+        sb.append(String.format("%15s: %5.2f€\n", "Preço", super.getPrice()));
+        sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", super.getFees()));
+        sb.append(String.format("%15s: %4.2f€\n", "Preço Final", getPriceWithFees()));
+        sb.append(String.format("%15s: %s\n", "Nome", getName()));
+        sb.append(String.format("%15s: %d\n", "NºContentores", getNumberOfContainers()));
+
+        return sb.toString();
     }
 }
